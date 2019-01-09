49cc6e578a0e34767d9fcb725b76100d2ed94a16
Caoanlong <caoanlong@gmail.com>
2019-01-09 17:25:43 +0800

    modify script

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main ()
 {
+    char *qs = getenv("QUERY_STRING");
+    char username[256];
+    sscanf(qs, "username=%s", username);
     printf("Content-Type:text/html;charset=utf-8\r\n\r\n");
-    printf("<html><head></head><body><font color='red'>曹安龙</font></body></html>");
+    printf("<html><head></head><body>");
+
+    if (strcmp(username, "admin") == 0)
+    {
+        printf("登录成功！");
+    }
+    else
+    {
+        printf("登录失败！");
+    }
+
+    printf("<font color='red'>用户名是：%s</font>", username);
+    printf("</body></html>");
     return 0;
 }
---
+++ script.sh
@@ -0,0 +1,7 @@
+gcc main.c -o main
+
+if [! -d "data" ];then mkdir target
+
+mv main.exe main.cgi
+
+mv main.cgi target
